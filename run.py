116bb15eea6ff5b5f6dc0f42d564780785374974
Sunil D S <sunil@zabbed.com>
2018-04-08 14:30:49 +0530

    index file and products API

--- run.py
+++ run.py
@@ -1,11 +1,28 @@
-from flask import Flask
+from flask import Flask, send_from_directory, request, Response
 
-app = Flask('mini-amazon')
+app = Flask('mini-amazon', static_url_path='')
 
 
 @app.route('/health', methods=['GET'])
 def health():
-    return 'healthy\n'
+    return 'healthy'
+
+
+@app.route('/', methods=['GET'])
+def index():
+    return send_from_directory('static', 'index.html')
+
+
+@app.route('/api/products', methods=['POST'])
+def products():
+    product = dict()
+    product['name'] = request.form['name']
+    product['description'] = request.form['description']
+    product['price'] = request.form['price']
+
+    print(product)
+
+    return Response('OK', 200)
 
 
 if __name__ == '__main__':
