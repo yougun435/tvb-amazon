a1f7d8895bf04ccfe9c22063aa5b72c2b7701ed3
Sunil D S <sunil@zabbed.com>
2018-04-08 13:02:34 +0530

    run

--- run.py
+++ run.py
@@ -1,1 +1,12 @@
-print('Hello World!')
+from flask import Flask
+
+app = Flask('mini-amazon')
+
+
+@app.route('/health', methods=['GET'])
+def health():
+    return 'healthy\n'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
