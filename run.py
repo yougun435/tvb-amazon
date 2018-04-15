0f3b8a4ce49967942ea15f3fd01f19195e33a634
Sunil D S <sunil@zabbed.com>
2018-04-15 11:53:59 +0530

    new files for admin and search

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 from flask import Flask, send_from_directory, request, Response
 from pymongo import MongoClient
+import re
 
 client = MongoClient('localhost', 27017)
 db = client.tvb_amazon
@@ -17,29 +18,35 @@ def index():
     return send_from_directory('static', 'index.html')
 
 
-@app.route('/api/products', methods=['POST', 'GET'])
+@app.route('/api/products', methods=['POST', 'GET', 'DELETE'])
 def products():
     if request.method == 'GET':
-        matching_prods = db.products.find({'name': request.args['name']})
+        query = {
+            'name': re.compile(request.args['name'], re.IGNORECASE)
+        }
+        matching_prods = db.products.find(query)
         matches = []
         for prod in matching_prods:
             matches.append(prod)
 
         return Response(str(matches), mimetype='application/json', status=200)
     elif request.method == 'POST':
-        product = dict()
-        product['name'] = request.form['name']
-        product['description'] = request.form['description']
-        product['price'] = request.form['price']
+        if request.form['op_type'] == 'insert':
+            product = dict()
+            product['name'] = request.form['name']
+            product['description'] = request.form['description']
+            product['price'] = request.form['price']
 
-        db.products.insert_one(product)
+            db.products.insert_one(product)
 
-        return Response(str({'status': 'success'}), mimetype='application/json', status=200)
+            return Response(str({'status': 'success'}), mimetype='application/json', status=200)
+        elif request.form['op_type'] == 'delete':
+            name = request.form['name']
+
+            db.products.delete_one(filter={'name': name})
+
+            return Response(str({'status': 'success'}), mimetype='application/json', status=200)
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
-
-json_obj = {
-    'name': ''
-}
+    app.run(host='0.0.0.0', port=5000, debug=True)
